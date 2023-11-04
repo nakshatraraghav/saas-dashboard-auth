aad3d829024a1daa7f511dc6a856e984ca219337
nakshatra <combatrickshaw@gmail.com>
2023-11-04 18:53:30 +0530

    (links): add links to the pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Main Page</main>;
+  return (
+    <main>
+      <ul className="flex flex-col space-y-4">
+        <li>
+          <Link className={buttonVariants()} href={"/login"}>
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link className={buttonVariants()} href={"/register"}>
+            Register
+          </Link>
+        </li>
+        <li>
+          <Link className={buttonVariants()} href={"/recover"}>
+            Recover
+          </Link>
+        </li>
+      </ul>
+    </main>
+  );
 }
